4f79acddf2ef69c77e673f5c0be7608b874633f7
Florent1711 <graphit01@hotmail.com>
2024-01-24 08:27:05 -0800

    ajout de 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    num_args = (len(argv) - 1)
+if num_args == 0:
+    print(f"{num_args} arguments.")
+else:
+    if num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for index, value in enumerate(argv[1:]):
+        print(f"{index + 1}: {value}")
