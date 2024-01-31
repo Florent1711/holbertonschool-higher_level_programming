f56c54d947d007fdfc877f373c27bbaecbc6811f
Florent1711 <graphit01@hotmail.com>
2024-01-31 17:15:02 +0100

    Ajout de 10-divisible_by_2

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if my_list is None or len(my_list) is 0:
         return None
-    max_value = my_list[0]
-    for num in my_list[1:]:
-        if num > max_value:
-            max_value = num
-            return max_value
+    max = my_list[0]
+    for i in range(len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
+    return max
