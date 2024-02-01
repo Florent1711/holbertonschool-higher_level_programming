ad5933d22a4a76833283e06547dbf615ef2c666b
Florent1711 <graphit01@hotmail.com>
2024-02-01 12:02:23 +0100

    modification 2-uniq_ad

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_set = set()
-    sum = 0
-    for num in my_list:
-        sum += num
-    return sum
+    result = 0
+    for x in set(my_list):
+        result += x
+    return result
