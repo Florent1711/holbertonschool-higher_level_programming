4511ebcce1cad4f3b3f7d4565e1cdb99f4654327
Florent1711 <graphit01@hotmail.com>
2024-02-01 11:33:40 +0100

    modification 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,5 @@ def uniq_add(my_list=[]):
     uniq_set = set()
     sum = 0
     for num in my_list:
-        if isinstance(num, int):
-            if num not in uniq_set:
-                uniq_set.add(num)
-                sum += num
+        sum += num
     return sum
