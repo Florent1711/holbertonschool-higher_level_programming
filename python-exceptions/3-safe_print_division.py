65fee6c258a0ae885c874756dba260dafe4c31df
Florent1711 <graphit01@hotmail.com>
2024-02-02 14:25:39 +0100

    modif de 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
-    # Assume that a and b are integers
     try:
-        # Try to divide a by b
-        result = a / b
-    except ZeroDivisionError:
-        # Handle the case when b is zero
-        result = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        # Print the result in the finally section
-        print("Inside result:", result)
-        # Return the result
-        return result
+        print("Inside result: {}".format(div))
+    return (div)
