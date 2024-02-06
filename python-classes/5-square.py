e417acd13a9c327272f58489a368012989b30caf
Florent1711 <graphit01@hotmail.com>
2024-02-06 16:01:41 +0100

    modification 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,7 +4,16 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize the square with a given size"""
-        self.size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+
 
     @property
     def size(self):
@@ -20,14 +29,9 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Return the area of the square"""
-        return self.__size ** 2
-
     def my_print(self):
         """Print the square with the character #"""
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                print("#" * self.__size)
+        for i in range(self.__size):
+            print("#" * self.__size)
