7b5de658cc375cd47985c5f399703d7e97830fed
Florent1711 <graphit01@hotmail.com>
2024-02-06 10:41:21 +0100

    modification de 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""A class Square that defines a square
+"""
+
+
 class Square:
-    """A class that represent a square"""
+    """create a class named Square
+    """
 
+    def __init__(self, size=0):
+        """__init__ method initializes an instance
+        Arg:
+            size: private instance attribute
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be greater or equal than zero
+        """
 
-    def __init__(self, size = 0):
-        """Initialize the square with an optionnal size"""
-
-        # check if size is an integer
-        if not isinstance(size, int):
-            #raise a TypeError exception with the message size
-            raise TypeError("Size must be an integer")
-        
-        # Assign the size to the private instance attribute
         self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
