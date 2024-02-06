0763cfef22fb3a2bad772d1f2e464b12316849e6
Florent1711 <graphit01@hotmail.com>
2024-02-06 14:03:54 +0100

    modification de 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""A class that represents a square"""
+"""Defines a class Square."""
 
 
 class Square:
-    """A class that defines a square by size and can compute area"""
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """Initializes a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        return self.__size * self.__size
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
