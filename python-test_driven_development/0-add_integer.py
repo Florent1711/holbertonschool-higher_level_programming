9500c4597bde1736ebaa59d74119134410a9d595
Florent1711 <graphit01@hotmail.com>
2024-02-09 11:06:29 +0100

    modification de 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,15 +3,16 @@
 This module adds only integers and returns them
 """
 
+
 def add_integer(a, b=98):
-    """This function adds two numbers that are only ints"""
-    #check if a or b is int or float or else raise TypeError
-    if type(a) != int and type(a) != float:
-        raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
-        raise TypeError("b must be an integer")
-    if type(a) == float:
+    """additionne deux nombres qui sont uniquement des entiers."""
+    # Vérifie si a ou b est un entier ou un flottant
+    if not isinstance(a, (int, float)):
+        raise TypeError("a doit être un entier")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b doit être un entier")
+    if isinstance(a, float):
         a = int(a)
-    if type(b) == float:
+    if isinstance(b, float):
         b = int(b)
-    return (a + b)
+    return a + b
