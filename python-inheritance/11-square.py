af7e0cf6d610f5d11988cafd305897af4b732a61
Florent1711 <graphit01@hotmail.com>
2024-02-15 15:06:41 +0100

    modification de 11-square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+This module contains a class that create a square by
+inheriting the class Rectangle
+"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """This class creates a square by inheriting
+    """
     def __init__(self, size):
-        self.integer_validator(size, size)
-        super().__init__(size, size)
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, size, size)
         self.__size = size
 
     def __str__(self):
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        return "[{}] {}/{}".format(
+                self.__class__.__name__, self.__size, self.__size)
