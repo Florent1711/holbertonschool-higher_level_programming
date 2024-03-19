be3d46ed653bfaf5d850a8e564e44e94a2edcc04
Florent1711 <graphit01@hotmail.com>
2024-03-19 13:39:12 +0100

    modif de 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,15 +1,12 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 
--- Check if the database exist and create it if not
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Select the Database to use
 USE hbtn_0d_usa;
 
--- Check if the table exist and create it if not
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO-INCREMENT,
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
