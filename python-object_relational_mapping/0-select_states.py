4dbc3119503d968400dd53f51c2488dec61d1574
Florent1711 <graphit01@hotmail.com>
2024-03-21 14:07:44 +0100

    Xeme modification de ce fichier de merde en langages de merde !

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
 
+#!/usr/bin/python3
+"""Module listing all states from the database"""
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="Angelina1711", db="hbtn_0e_0_usa")
-    cur = conn.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    connexion = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    curseur = connexion.cursor()
+    curseur.execute("""SELECT * FROM states ORDER BY id""")
+    rows = curseur.fetchall()
+    for row in rows:
         print(row)
-
-    cur.close()
-    conn.close()
+    curseur.close()
+    connexion.close()
