dcae8a4fa23be6545f1f0f734ab25c573300a35f
Florent1711 <graphit01@hotmail.com>
2024-03-21 16:21:43 +0100

    ce putain d'e'xercice de merde sur du logiciel qui vienbt du linux de merde, vois les incompatibilités !!!! Ne jamlais prendre linux, quand tout le monde a accès au code, c'est le contraire de la sécurité !!!

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,17 @@
+
 #!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - task 0. Get all states"""
+'''
+Get all states
+'''
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+import MySQLdb
+from sys import argv
 
-    if len(sys.argv) != 4:
-        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
-                 ' <database name>')
-
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
