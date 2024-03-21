89e7e3c91a05633071caaabf82d3c2e9ff4e32b4
Florent1711 <graphit01@hotmail.com>
2024-03-21 12:05:09 +0100

    modif de 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,23 @@
-#!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - task 0. Get all states"""
+import MySQLdb
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
-
-    if len(sys.argv) != 4:
-        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
-                 ' <database name>')
-
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+def list_states(username, password, database):
+    # Connect to the MySQL server
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
     cur = conn.cursor()
+
+    # Execute the query to get all states
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+
+    # Fetch the results
+    results = cur.fetchall()
+
+    # Print the results
+    for row in results:
         print(row)
+
+    # Close the cursor and connection
     cur.close()
     conn.close()
-    
+
+# Call the function with the arguments
+list_states('username', 'password', 'hbtn_0e_0_usa')
