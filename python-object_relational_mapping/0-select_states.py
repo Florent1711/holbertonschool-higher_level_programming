f1aa9c1e627036c37e039f9eaac156d5624e3525
Florent1711 <graphit01@hotmail.com>
2024-03-21 15:44:39 +0100

    remodification de ce fichier de

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
-
 #!/usr/bin/python3
-"""Module listing all states from the database"""
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
-    connexion = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-    )
-    curseur = connexion.cursor()
-    curseur.execute("""SELECT * FROM states ORDER BY id""")
-    rows = curseur.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    curseur.close()
-    connexion.close()
+    cur.close()
+    conn.close()
