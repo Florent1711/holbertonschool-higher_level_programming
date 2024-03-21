9ba857601a11406fa3252ce1427da336b49f8367
Florent1711 <graphit01@hotmail.com>
2024-03-21 12:11:56 +0100

    ce langage de merde !!!!!

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 from sys import argv
 
-def list_states(username, password, db_name):
-    """
-    Lists all states from the database.
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
 
 if __name__ == "__main__":
-    if len(argv) == 4:
-        list_states(argv[1], argv[2], argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
