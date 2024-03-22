7d837965206ef4640ec25c8544817b4cb4ecc8aa
Florent1711 <graphit01@hotmail.com>
2024-03-22 15:39:08 +0100

    modif de 4-cities_by_states

--- python-object_relational_mapping/4-cities_by_states.py
+++ python-object_relational_mapping/4-cities_by_states.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""Display cities"""
+
+"""
+File: 4-cities_by_state.py
+Desc: This module contains a python script that lists all
+cities from the database hbtn_0e_4_usa.
+
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Oct 7 2022
+"""
+
+from sys import argv
 import MySQLdb
-import sys
-
-
-def list_cities():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ASC")
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-
-    cur.close()
-    db.close()
-
 
 if __name__ == "__main__":
-    list_cities()
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name,
+            states.name FROM cities
+            INNER JOIN states
+            ON cities.state_id = states.id
+            ORDER BY cities.id ASC""")
+    states_info = cur.fetchall()
+
+    for state in states_info:
+        print(state)
