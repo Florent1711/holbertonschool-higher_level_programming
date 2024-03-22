ad990726304290283beb25abf56a7115a9e5e381
Florent1711 <graphit01@hotmail.com>
2024-03-22 14:08:14 +0100

    modif de 0-select

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,14 @@
 """lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           password=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           password=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
