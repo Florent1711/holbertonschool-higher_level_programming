f6db6c4d205c58dd3e01e34fda83cc29695b6ee1
Florent1711 <graphit01@hotmail.com>
2024-03-22 17:20:49 +0100

    modif de 4-cities_by_states

--- python-object_relational_mapping/4-cities_by_states.py
+++ python-object_relational_mapping/4-cities_by_states.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-
 """
-File: 4-cities_by_state.py
-Desc: This module contains a python script that lists all
-cities from the database hbtn_0e_4_usa.
-
-Author: Gizachew Bayness (Elec Crazy)
-Date Created: Oct 7 2022
+List all cities from a database
 """
-
-from sys import argv
+import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name,
-            states.name FROM cities
-            INNER JOIN states
-            ON cities.state_id = states.id
-            ORDER BY cities.id ASC""")
-    states_info = cur.fetchall()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
 
-    for state in states_info:
+    for state in states:
         print(state)
